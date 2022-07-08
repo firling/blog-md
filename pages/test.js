efeb3a7210cc5dcaaea8cccaaec100895164b00d
Julien Anquetil <ja@trinum.com>
2022-07-08 14:38:01 +0200

    editor start

--- pages/test.js
+++ pages/test.js
@@ -1,17 +1,79 @@
-import React from "react";
-import MDX from "@mdx-js/runtime";
+import { createStyles, Textarea } from '@mantine/core';
+import { useState } from 'react';
+import MDX from '@mdx-js/runtime';
 
-const components = {
-  h1: props => <h1 style={{ color: "tomato" }} {...props} />,
-  Demo: () => <h1>This is a demo component</h1>
-};
+import {ErrorBoundary} from 'react-error-boundary'
 
-const mdx = `
-# Hello, MDX Runtime!
+import components from '../utils/components'
 
-Hey look, this resolves the "@mdx-js/runtime" package now!!!
+const useStyles = createStyles(() => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100%'
+  },
+  child: {
+    flex: '1',
+    padding: 30
+  },
+  child_editor: {
+    display: 'flex',
+    alignItems: 'stretch',
+  },
+  child_preview: {
+    border: '1px solid black',
+    overflowY: 'scroll',
+  },
+}));
+
+function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+export default function Test() {
+  const { classes, cx } = useStyles();
+  const [mdx, setMdx] = useState(`# Hello, world!
 
 <Demo />
-`;
 
-export default () => <MDX components={components}>{mdx}</MDX>;
+<div>Here is the scope variable: </div>`);
+
+  const component = {
+    h1: props => <h1 style={{color: 'tomato'}} {...props} />,
+    Demo: props => <h1>This is a demo component</h1>,
+    ...components
+  }
+
+
+  return (
+    <div className={classes.container}>
+      <div className={cx(classes.child, classes.child_editor)}>
+        <Textarea
+          placeholder="Autosize with no rows limit"
+          autosize
+          size="xl"
+          minRows={1000}
+          styles={{
+            root: { width: '100%', height: '100%' },
+            wrapper: { height: '100%' },
+          }}
+          value={mdx}
+          onChange={(e) => setMdx(e.currentTarget.value)}
+        />
+      </div>
+      <div className={cx(classes.child, classes.child_preview)}>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+        >
+          <MDX components={component}>{mdx}</MDX>
+        </ErrorBoundary>
+      </div>
+    </div>
+  );
+}
